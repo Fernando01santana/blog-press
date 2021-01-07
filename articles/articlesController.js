5a76dad41f16cbf6def17737fc4e99e4b2c36622
Fernando01santana <cool.fernando23@gmail.com>
2021-01-07 18:58:04 -0300

    add: inset and delete category

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('./article');
+const slugify = require('slugify');
 
 router.get('/articles',(req,res)=>{
-    res.send('Pagina de artigos')
+    res.send('Pagina de artigos');
 })
 router.get('/admin/articles/new',(req,res)=>{
-    res.send('Painel de admin')
+    res.render("admin/articles/new");
+})
+
+router.post('/article/save',(req,res)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+
+    if((title != undefined) && (body != undefined)){
+        Article.create({
+            title:title,
+            slug:slugify(title),
+            body:body
+        }).then(()=>{
+            console.log("insert category");
+            res.redirect("/")
+        })
+    }else{
+        res.redirect("/admin/articles/new")
+    }
 })
 module.exports = router;
--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,8 +1,48 @@
 const express = require('express');
+const Category = require('./category')
+const slugify = require('slugify');
 const router = express.Router();
 
 router.get("/admin/categories/new",(req,res)=>{
     res.render('admin/categories/new')
 })
 
+router.get("/admin/categories",(req,res)=>{
+    Category.findAll().then(categories=>{
+        res.render("admin/categories/index.ejs",{categories:categories});
+    })
+})
+
+router.post('/categories/delete',(req,res)=>{
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where:{
+                    id:id
+                }
+            }).then(()=>{
+                res.redirect('/admin/categories')
+            })
+        }else{
+            res.redirect('/admin/categories')
+        }
+    }else{
+        res.redirect('/admin/categories/')
+    }
+})
+//pega os dados do formulario e salva no banco de dados
+router.post("/categories/save",(req,res)=>{
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title:title,
+            slug:slugify(title)
+        }).then(()=>{
+            res.redirect("/")
+        })
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+})
 module.exports = router;
